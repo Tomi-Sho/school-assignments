1aaf3e2322a2d9cbc3a8e465f2890346f206ddee
Tomi-Sho <koume.majika+work@gmail.com>
2026-06-24 11:51:01 +0900

    埼玉県南部のLINE天気通知BotのPythonファイルを追加

---
+++ weather_line_bot/README.md
@@ -0,0 +1,13 @@
+# LINE天気通知Bot
+
+Pythonでの初めての自主学習としてLINE天気通知Botをつくってみました。LINE CHANNEL ACCESS TOKENとUSER IDを変えることで埼玉県南部の天気を公式LINEで送信できます。
+
+## 特徴
+- GitHubで定期更新もつくる予定です。
+
+## 動作環境
+- **OS:** Windows 10 / 11 , macOS（基本OSに依存せず動作可能）
+- **Python:** 3.10+
+- **使用ライブラリ:**
+    - `requests`
+    - `line-bot-sdk`
---
+++ weather_line_bot/weather_line.py
@@ -0,0 +1,51 @@
+import os
+import requests
+import datetime
+from linebot import LineBotApi
+from linebot.models import TextSendMessage
+
+# 今日の月と日を取得
+now_time = datetime.date()
+
+# LINEのChannelAccessTokenとUSER IDを取得する
+LINE_CHANNEL_ACCESS_TOKEN = os.getenv("LINE_CHANNEL_ACCESS_TOKEN")
+USER_ID = os.getenv("USER_ID")
+
+line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
+
+# 気象庁から天気情報を取得
+url = "https://www.jma.go.jp/bosai/forecast/data/forecast/110000.json" # 熊谷地方気象台
+
+try:
+    response = requests.get(url)
+    response.raise_for_status()
+    weather_data =response.json()
+
+    # 地域名と天気情報を抜き出す
+    area_name = weather_data[0]["timeSeries"][0]["areas"][1]["area"]["name"] # 南部
+    today_weather = weather_data[0]["timeSeries"][0]["areas"][1]["weathers"][0] # 南部
+
+    # 天気に記号をつける
+    if "雷" in today_weather:
+        today_weather += "⛈"
+    elif "雪" in today_weather:
+        today_weather += "⛄"
+    elif "雨" in today_weather:
+        today_weather += "⛈"
+    elif "くもり" in today_weather and "晴れ" in today_weather:
+        today_weather += "🌤のち曇り"
+    elif "くもり" in today_weather:
+        today_weather += "☁"
+    elif "晴れ" in today_weather:
+        today_weather += "☀"
+
+    # LINEに送信するメッセージの作成
+    line_message = f"【今日の天気】\n{now_time.month}月{now_time.day}日の埼玉県 {area_name}の天気は「{today_weather}です！」" 
+    
+    # LINEにメッセージを送信
+    line_bot_api.push_message(USER_ID, TextSendMessage(text=line_message))
+    print("LINEへの天気通知が成功しました")
+
+# エラー処理
+except Exception as e:
+    print(f"エラーが発生しました：{e}")
